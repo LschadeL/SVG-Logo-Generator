1d6999b012836bc460e258fa026bfc7c21a67c42
Lillith S <loganniles96@gmail.com>
2024-01-18 05:21:57 -0600

    adding inquirer questions array

--- Output/server.js
+++ Output/server.js
@@ -1,2 +1,35 @@
 const inquirer = require('inquirer');
 const classes = require('../Assets/classes');
+
+// creating an array of question to construct the SVG logo
+
+const questions = [{
+    type: "list",
+    name: "shape",
+    message: "What shape is your logo?",
+    choices: ["Circle", "Square", "Triangle"]
+},
+{
+    type: "input",
+    name: "letters",
+    message: "Please type which three letters you would like to display inside the logo."
+},
+{
+    type: "list",
+    name: "letter-color",
+    message: "What color lettering should be generated?",
+    choices: ["White", "Black", "Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+},
+{
+    type: "list",
+    name: "shape-color",
+    message: "What color shape should be generated?",
+    choices: ["White", "Black", "Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+}]
+
+function init() {
+    inquirer.prompt(questions)
+.then((inquirerAnswers) => {
+console.log(inquirerAnswers);
+});
+}
